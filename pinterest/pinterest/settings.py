e13bac7da06fd163fc549ddf7e01c29c0ec46cfd
woomae <cwg0809@naver.com>
2023-03-28 22:03:55 +0900

    django course 11 commit

--- pinterest/pinterest/settings.py
+++ pinterest/pinterest/settings.py
@@ -129,8 +129,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS =[
+    BASE_DIR / "static",
+]
