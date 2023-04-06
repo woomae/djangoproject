7ede6317685d02ba338e95ea62f1e5a07c6c1cfa
woomae <cwg0809@naver.com>
2023-04-06 14:58:01 +0900

    17

--- pinterest/accountapp/views.py
+++ pinterest/accountapp/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from accountapp.models import HelloWWorld
 
 # Create your views here.
 
 def hello_world(request):
     
     if request.method == "POST":
-        return render(request, 'accountapp/hello_world.html', context={'text':'POST METHOD!!!'})
+        temp=request.POST.get('hello_world_input')
+        new_hello_world = HelloWWorld()
+        new_hello_world.text=temp
+        new_hello_world.save()
+                
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
     else:
         return render(request, 'accountapp/hello_world.html', context={'text':'GET METHOD!!!'})
         
