00260059fa3b3e824044a57642a784fe523b5a9f
woomae <cwg0809@naver.com>
2023-04-06 15:14:20 +0900

    18

--- pinterest/accountapp/views.py
+++ pinterest/accountapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import HttpResponseRedirect, render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from accountapp.models import HelloWWorld
 
 # Create your views here.
@@ -11,9 +12,10 @@ def hello_world(request):
         new_hello_world = HelloWWorld()
         new_hello_world.text=temp
         new_hello_world.save()
-                
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
+        
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
-        return render(request, 'accountapp/hello_world.html', context={'text':'GET METHOD!!!'})
+        hello_world_list = HelloWWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
         
 
