62c275fc60cc5ad433c49137dda61881dda8d852
woomae <cwg0809@naver.com>
2023-04-09 13:23:33 +0900

    21

--- pinterest/accountapp/views.py
+++ pinterest/accountapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import HttpResponseRedirect, render
 from django.http import HttpResponse, HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from accountapp.models import HelloWWorld
 
 # Create your views here.
@@ -19,3 +22,8 @@ def hello_world(request):
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
         
 
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp: hello_world')
+    template_name = 'accountapp/create.html'
