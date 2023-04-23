7d0a357b0fb92154e499511baddfb36fcb831af1
woomae <cwg0809@naver.com>
2023-04-23 23:29:17 +0900

    26

--- pinterest/accountapp/views.py
+++ pinterest/accountapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import HttpResponseRedirect, render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, DetailView,UpdateView
+from django.views.generic import CreateView, DetailView,UpdateView, DeleteView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from accountapp.models import HelloWWorld
@@ -39,3 +39,9 @@ class AccountUpdateView(UpdateView):
     form_class = AccountUpdateForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
+    
+class AccountDeleteView(DeleteView):
+    model = User
+    success_url = reverse_lazy('accountapp:login')
+    template_name = 'accountapp/delete.html'
+    
