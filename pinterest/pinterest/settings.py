d761befadf8b257f592310413717bf1a2841f63c
woomae <cwg0809@naver.com>
2023-04-24 23:41:44 +0900

    fix 29 maybe

--- pinterest/pinterest/settings.py
+++ pinterest/pinterest/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
@@ -142,3 +142,6 @@ STATICFILES_DIRS =[
 ]
 LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
 LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
