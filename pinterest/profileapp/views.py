e40c938b4553aecbd51e9a90e1303a0c228db164
woomae <cwg0809@naver.com>
2023-05-04 12:42:25 +0900

    32

--- pinterest/profileapp/views.py
+++ pinterest/profileapp/views.py
@@ -1,8 +1,10 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
+from django.utils.decorators import method_decorator
+from profileapp.decorators import profile_ownership_required
 
 # Create your views here.
 
@@ -19,3 +21,12 @@ class ProfileCreateView(CreateView):
         temp_profile.user = self.request.user
         temp_profile.save()   
         return super().form_valid(form)
+
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    context_object_name = 'target_profile'
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/update.html'
