f4490924a75159fd1ef203b8c002e097071e9f3a
woomae <cwg0809@naver.com>
2023-05-04 15:49:45 +0900

    33

--- pinterest/profileapp/views.py
+++ pinterest/profileapp/views.py
@@ -1,4 +1,4 @@
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, UpdateView
 
 from profileapp.forms import ProfileCreationForm
@@ -13,7 +13,6 @@ class ProfileCreateView(CreateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/create.html'
     
     def form_valid(self, form):
@@ -21,6 +20,9 @@ class ProfileCreateView(CreateView):
         temp_profile.user = self.request.user
         temp_profile.save()   
         return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk':self.object.user.pk})
 
 @method_decorator(profile_ownership_required, 'get')
 @method_decorator(profile_ownership_required, 'post')
@@ -30,3 +32,6 @@ class ProfileUpdateView(UpdateView):
     form_class = ProfileCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/update.html'
+    
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk':self.object.user.pk})
